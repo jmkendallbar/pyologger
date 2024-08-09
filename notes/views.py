4dd1fbbb128643185ebdcd1c2847e09c7c87bee3
Jessica Kendall-Bar <jkb@ucsc.edu>
2024-08-09 09:57:53 -0700

    updating some django items

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Notes
+
+def list(request): # making a function to view all notes
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
